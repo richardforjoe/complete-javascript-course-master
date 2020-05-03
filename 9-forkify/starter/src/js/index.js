ae8368457392eb3ad2dd97100d385554ce908288
Richard Forjoe <richard.forjoe@sky.uk>
2020-05-03 13:52:31 +0100

    First part done

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,4 +1,6 @@
 // Global app controller
-import test from './test';
-const x = 23;
-console.log(`I imported ${test} from another module. Variable in this module x is ${x}`)
+import string from './models/Search'; //import an exported value, current folder + module name
+//import {add as addition, multiply, ID} from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${string}`)
